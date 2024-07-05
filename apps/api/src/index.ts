9180fb88bbd7e126443123d67321101f6564db8a
victormicco <victor.oficial093@gmail.com>
2024-07-05 17:27:19 -0300

    fix(turborepo): vercel deployment

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,4 @@
-import Fastify from "fastify";
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { rootRouter } from "./routes/root";
 import { RootService } from "./services/root";
 
@@ -6,11 +6,15 @@ export const app = Fastify();
 
 app.register((app) => rootRouter(app, new RootService()));
 
-const host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
+// const host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
 
-const url = await app.listen({
-  port: 3002,
-  host,
-});
+// const url = await app.listen({
+//   port: 3002,
+//   host,
+// });
+export default async (req: FastifyRequest, res: FastifyReply) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
 
-console.log(`Server listening on ${url} 🚀`);
+console.log(`Server listening on  🚀`);
