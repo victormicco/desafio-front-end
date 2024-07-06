5592e99c514f45555bae6f4795a40a944727f5da
victormicco <victor.oficial093@gmail.com>
2024-07-06 20:58:06 -0300

    fix(routes): redirect to dashboard

--- apps/web/src/utils/sidebar-routes.ts
+++ apps/web/src/utils/sidebar-routes.ts
@@ -3,23 +3,23 @@ import { BarChartBig, FilePieChart } from "lucide-react";
 type IconComponent = React.FC<React.SVGProps<SVGSVGElement>>;
 
 interface SidebarRoutesType {
-	id: number;
-	name: string;
-	path: string;
-	icon: IconComponent;
+  id: number;
+  name: string;
+  path: string;
+  icon: IconComponent;
 }
 
 export const sidebarRoutes: SidebarRoutesType[] = [
-	{
-		id: 1,
-		name: "Dashboard",
-		path: "/",
-		icon: FilePieChart,
-	},
-	{
-		id: 2,
-		name: "Leaderboard",
-		path: "/leaderboard",
-		icon: BarChartBig,
-	},
+  {
+    id: 1,
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: FilePieChart,
+  },
+  {
+    id: 2,
+    name: "Leaderboard",
+    path: "/leaderboard",
+    icon: BarChartBig,
+  },
 ];
